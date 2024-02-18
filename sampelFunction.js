c5b25d2478146de1d164f9ab2f2c5e56ca2cd61a
MarkRywell <markgaje43@gmail.com>
2024-02-18 17:35:57 +0800

    Add new code

--- sampelFunction.js
+++ sampelFunction.js
@@ -2,6 +2,10 @@ const sampleFunction = () => {
     console.log('This is a sample function');
 };
 
+const sampleFunction1 = () => {
+    console.log('This is a sample function');
+}
+
 module.exports = {
     sampleFunction
 }
